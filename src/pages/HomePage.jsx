98ee3b14762cfb31eb3365ee136046a67c6149ed
Johoane <axelmonde7@gmail.com>
2025-07-03 12:23:46 +0200

    Add home page components

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,21 @@
+import HeroSection from "../components/HeroSection";
+import AProposSection from "../components/AproposSection";
+import NewAlbumSection from "../components/NewAlbumSection";
+import ConcertSection from "../components/ConcertSection";
+import BlogSection from "../components/BlogSection";
+import Footer from "../components/Footer";
+
+const HomePage = () => {
+  return (
+    <>
+      <HeroSection />
+      <AProposSection />
+      <NewAlbumSection />
+      <ConcertSection />
+      <BlogSection />
+      <Footer />
+    </>
+  );
+};
+
+export default HomePage;
